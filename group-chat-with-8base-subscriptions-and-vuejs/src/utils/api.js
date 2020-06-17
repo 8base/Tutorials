520c6e8ccab311570c8f46f5c2e0b36a8cbeb230
Sebastian Scholl <sebscholl@gmail.com>
2020-06-17 16:50:02 -0400

    stripped down component for sake of tutorial. Completed code is in public-group-chat brancg

--- group-chat-with-8base-subscriptions-and-vuejs/src/utils/api.js
+++ group-chat-with-8base-subscriptions-and-vuejs/src/utils/api.js
@@ -1,15 +1,3 @@
-import { ApolloClient } from "apollo-boost";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-
-const { VUE_APP_8BASE_API_ENDPOINT } = process.env;
-
-export default new ApolloClient({
-  link: createHttpLink({
-    uri: VUE_APP_8BASE_API_ENDPOINT,
-  }),
-  cache: new InMemoryCache(),
-  defaultOptions: {
-    fetchPolify: 'no-cache'
-  }
-});
+/**
+ * Initialize ApolloClient in here.
+ */
